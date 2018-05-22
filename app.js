24d6f38c46ab78be2689bbeddb8d1dd6a3e94716
Andrew Peterson <16apeterson@gmail.com>
2018-05-21 23:46:25 -0500

    added new functions to pull new data

--- app.js
+++ app.js
@@ -6,23 +6,62 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+
+
+  const message = "!ocia feeds"
+  const worker = Worker(message);
+  worker.getFeed( (err, res) => {
+    if (err) console.log(err);
+    // console.log(res);
+  });
+
 });
 
 client.on('message', msg => {
-
-  if (msg.author.username === 'TheMiniPete') {
-    msg.delete()
-    .then(msg => console.log(`Deleted message from ${msg.author.username}`))
-    .catch(console.error);
-  }
-
   const regex = /^([!])([A-Za-z0-9])\w+\s([FEDSfeds])\w+/gm;
   if ( regex.test(msg.content) ) {
-    const worker = Worker(msg);
     console.log(`Query -- ${msg.content}`);
 
+    const worker = Worker(msg);
+    worker.getFeed( (err, res) => {
+      if (err) console.log(err);
+      // console.log(res);
+    });
 
+  }
+
+  if (msg.author.username === 'Robbie' || msg.author.username === 'Sajirodman11') {
+    let ranNum =  Math.floor(Math.random() * (50 - 1 + 1));
+    console.log(`Random guess -- ${ranNum}`);
+    if ( ranNum === 5 ) {
+      msg.channel.send('bice nind');
+    }
   }
 });
 
 client.login(resources.discord_token);
+
+// detect roles
+// if(!message.member.roles.find("name", "Big Boi") && !message.member.roles.find("name", "Supreme Big Boi")){
+//     message.reply("");
+//     return;
+// }\
+
+// message.guild.members.find(val => val.user.username === 'USERNAME')
+//   msg.delete().then(msg => console.log(`Deleted message from ${msg.author.username}`))
+// .catch(console.error);
+// msg.channel.send('+tts Robbie bice nind', {tts:true}).then(message => message.delete(1));
+
+// message.member.voiceChannel.join().then(connection => connection.playArbitraryInput(ytdl(splitMessage[1], {
+//             filter: 'audioonly'
+//         })).then(dispatcher => {
+//         dispatcher.on('error', message.send("There was an error playing your file"))
+//     }));
+
+
+// leagueAPI.getMatch(response['matches'][match]['gameId']).then(response => {
+// leagueAPI.getMatchlist(accountId, {
+//     query: {
+//         endIndex: 20
+//     }
+// }).then(response => {
--- src/worker.js
+++ src/worker.js
@@ -2,28 +2,96 @@ const request = require('request');
 const resources = require('./resources.js');
 
 const worker = (msg) => {
-  const summonerInfo = {
-    'api_key': resources.riot_api_key,
-    'base_link': 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/'
+
+  /**
+    @private
+  */
+  const _urlInfo = {
+    'base_link': 'https://na1.api.riotgames.com/lol/',
+    'api_key': resources.riot_api_key
   };
 
-  const summonerUrl = `${summonerInfo.base_link}`
-                    + `${msg.content.replace('!', '').split(' ')[0]}`
-                    + `?api_key=${summonerInfo.api_key}`;
+  /**
+    @private
+  */
+  const _build_api_url = (type, id, query) => {
+    return `${_urlInfo.base_link}${type}${id}?`
+            +`${(query) ? query + '&': ''}api_key=${_urlInfo.api_key}`;
+  };
 
-  const getAccId = (callback) => {
-    request.get(
-      { url: summonerUrl, json: true },
-      (err, res, body) => {
-        if (err) {} // return callback  }
-        // return callback(body.accountId);
-        resolve(body.accoutnId);
+  /**
+    Returns account ID of user provided
+    @private
+  */
+  const _getAccId = (callback) => {
+    // const summonerUrl = _build_api_url('summoner/v3/summoners/by-name/', msg.content.replace('!', '').split(' ')[0], null);
+    const summonerUrl = _build_api_url('summoner/v3/summoners/by-name/', msg.replace('!', '').split(' ')[0], null);
+
+    request({ url: summonerUrl, json: true }, (err, res, body) => {
+        if (err) { return callback(err, {'statusCode': res.statusCode} ); }
+        return callback(null, body.accountId);
       }
     );
+  };
+
+  /**
+    @private
+  */
+  const _getMatchList = (accountId, callback) => {
+    const matchListUrl = _build_api_url('match/v3/matchlists/by-account/', accountId, 'endIndex=10');
+    request({ url: matchListUrl, json: true }, (err, res, body) => {
+        if (err) { return callback(err, {'statusCode': res.statusCode} ); }
+        return callback(null, body);
+      }
+    );
+  };
+
+  /**
+    @private
+  */
+  const _getMatches = (name, matchList, callback) => {
+    const games = []
+    for (const i in matchList) {
+      games.push(matchList[i].gameId);
+    }
+
+    games.forEach( gameId => {
+      const gameUrl = _build_api_url('match/v3/matches/', gameId, null);
+      request({ url: gameUrl, json: true }, (err, res, body) => {
+        if (err) { return callback(err, { 'statusCode': res.statusCode }); }
+
+        let participant = body.participantIdentities.filter( p => (p.player.summonerName === name))
+
+        let participantID = undefined;
+        body.participantIdentities.forEach(p => { if (p.player.summonerName === name) participantID = p.participantId } );
+        console.log(participantID);
+
+        body.stats
+
+        // console.log(body.participantIdentities.forEach( p => console.log(p.player.summonerName)));
+
+      });
+    });
+    // const games = matchList.filter
+  }
+
+  const getFeed = (callback) => {
+    // const summoner = msg.content.replace('!', '').split(' ')[0];
+    const summoner = msg.replace('!', '').split(' ')[0];
+    _getAccId( (acc_err, acc_res) => {
+      if (acc_err) { return callback(acc_err, null); }
+      _getMatchList(acc_res, (matchList_err, matchList_res) => {
+        if (matchList_err) { return callback(matchList_err, null); }
+        _getMatches(summoner, matchList_res.matches, (match_err, match_res) => {
+          if(match_err) { return callback(matchList_err, null); }
+
+        });
+      });
+    });
   }
 
   return {
-    getAccId
+    getFeed
   }
 };
 
